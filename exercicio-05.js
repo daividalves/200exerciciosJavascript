e2235d5bd02d97052daec21f4a3f024d471d3039
daividalves <daividgpt@gmail.com>
2025-09-15 10:21:16 -0300

    feat: adiciona exercicio 5 sobre operador de adicao

---
+++ exercicio-05.js
@@ -0,0 +1,10 @@
+//Declare duas variáveis e atribua valores numéricos a elas. Use o operador de adição para realizar operações entre as duas variáveis. Imprima o resultado.
+
+
+let numero1 = 10;
+let numero2 = 5.2;
+
+let resultado = numero1 + numero2;
+
+console.log(resultado);
+
