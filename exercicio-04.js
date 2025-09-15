db55358d1e559a80224dea04ae8e3c39414a9b83
daividalves <daividgpt@gmail.com>
2025-09-15 10:20:02 -0300

    feat: adiciona exercicio 4 sobre concatenacao de strings

---
+++ exercicio-04.js
@@ -0,0 +1,8 @@
+//Declare duas variáveis chamadas "nome" e "sobrenome", atribua o seu nome e sobrenome a elas. Concatene-as em uma terceira variável chamada "nomeCompleto" e imprima.
+
+let nome = 'Daivid';
+let sobrenome = 'Alves';
+
+const nomeCompleto = nome + ' ' + sobrenome;
+
+console.log(nomeCompleto);
