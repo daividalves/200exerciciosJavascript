dbd387d62c09e3a8518f127f209ea85812930167
daividalves <daividgpt@gmail.com>
2025-09-15 10:19:06 -0300

    feat: adiciona exercicio 3 sobre valor maximo numerico

---
+++ exercicio-03.js
@@ -0,0 +1,6 @@
+//Declare uma variável chamada "númeroMáximo", atribua a ela o valor máximo que um número pode ter em JavaScript e imprima o valor na console.
+
+
+let numeroMaximo = Number.MAX_VALUE;
+
+console.log(numeroMaximo);
