2a609ba74a91b47f14e2f0f08efc6169fe8951ff
Daivid Alves <daividgpt@gmail.com>
2025-09-20 02:25:01 -0300

    feat: Implementa comparações de valores numéricos

---
+++ exercicio-07.js
@@ -0,0 +1,44 @@
+/*
+ Declare três variáveis e atribua valores numéricos a elas. 
+ 
+ Use operadores de comparação para comparar os valores entre as variáveis. Imprima os resultados.
+
+*/
+
+
+// Declarando e atribuindo valores numéricos
+const a = 10;
+const b = 25;
+const c = 10;
+
+// Operador de igualdade (==)
+console.log(`a == b: ${a == b}`); // Saída: false
+console.log(`a == c: ${a == c}`); // Saída: true
+
+// Operador de igualdade estrita (===)
+console.log(`a === c: ${a === c}`); // Saída: true
+
+// Operador de desigualdade (!=)
+console.log(`a != b: ${a != b}`); // Saída: true
+console.log(`a != c: ${a != c}`); // Saída: false
+
+// Operador de desigualdade estrita (!==)
+console.log(`a !== c: ${a !== c}`); // Saída: false
+
+// Operador "maior que" (>)
+console.log(`b > a: ${b > a}`); // Saída: true
+console.log(`a > b: ${a > b}`); // Saída: false
+
+// Operador "menor que" (<)
+console.log(`a < b: ${a < b}`); // Saída: true
+console.log(`b < a: ${b < a}`); // Saída: false
+
+// Operador "maior ou igual a" (>=)
+console.log(`a >= c: ${a >= c}`); // Saída: true
+console.log(`b >= a: ${b >= a}`); // Saída: true
+
+// Operador "menor ou igual a" (<=)
+console.log(`a <= c: ${a <= c}`); // Saída: true
+console.log(`a <= b: ${a <= b}`); // Saída: true
+
+
