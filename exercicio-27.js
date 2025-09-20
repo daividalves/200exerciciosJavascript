b6aefc5b57300ffc6e902e13e3b7f547f707af1b
Daivid Alves <daividgpt@gmail.com>
2025-09-20 09:19:58 -0300

    feat: Cria programa de tabuada de multiplicação com laço for

---
+++ exercicio-27.js
@@ -0,0 +1,11 @@
+//Usando um laço for, imprima a tabuada de multiplicação do número 5.
+
+const numero = 5;
+
+for (let i = 1; i <= 10; i++) {
+
+    const resultado = numero * i;
+
+    console.log(`${numero} x ${i} = ${resultado}`);
+
+}
