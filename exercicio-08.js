fae31d9b40181f39ce4a449fe974cbeebfd63d29
Daivid Alves <daividgpt@gmail.com>
2025-09-20 02:28:18 -0300

    feat: Demonstra o uso do operador de incremento

---
+++ exercicio-08.js
@@ -0,0 +1,14 @@
+/*
+
+Declare uma variável e atribua um valor numérico a ela. Use o operador de incremento para aumentar o valor da variável. 
+
+Imprima o resultado.
+
+*/
+
+
+let numero = 10;
+
+numero++;
+
+console.log(`O valor da variável após o incremento é: ${numero}`); // Saída: O valor da variável após o incremento é: 11
